2e79d3898d07e824ef714dee0affa5d67fad46df
lizitang <1650731623@qq.com>
2018-08-24 15:45:07 +0800

    vue-router example

---
+++ vue_project/vue-router_demo/src/components/page01.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h2 class="text-center">{{msg1}}</h2>
+        <ul class="text-center spacing" style="padding:0;">
+            <li v-for="item in links"><a @click="$goRoute(item.route)">{{item.text}}</a></li>
+        </ul>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'page01',
+  data () {
+    return {
+        msg1: '这里是第一个页面',
+        links: [
+            {
+                text: '圆白菜',
+                route: '/page01/page01-a'
+            },
+            {
+                text: '长白菜',
+                route: '/page01/page01-b'
+            }
+        ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
---
+++ vue_project/vue-router_demo/src/components/page02.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="text-center">{{msg2}}</div>
+  </div> 
+</template>
+
+<script>
+export default {
+  name: '',
+  data () {
+    return {
+      msg2: '好好学小半别闹'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
